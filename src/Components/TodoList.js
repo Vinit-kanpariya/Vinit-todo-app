105eafc5c2fac7edb576c5794b0b68b159880317
Vinit kanpariya <vinit.k@solutelabs.com>
2024-12-09 15:39:19 +0530

    Update TodoList.js

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import TodoItem from "./TodoItem"; 
 
-function TodoList({todos, toggleComplete, deleteTodo}){
+function TodoList({todos, toggleComplete, deleteTodo, editTodo}){
     return(
         <div className="w-full max-w-md bg-white rounded-lg shadow-md p-4">
         {todos.map((todo) => (
             <TodoItem key={todo.id} todo={todo}
             toggleComplete={toggleComplete}
-            deleteTodo={deleteTodo}/>
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}/>
         ))}
         </div>
     );
